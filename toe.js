f3af85ef2071feaa2213cb4cf30fc3fbe85aa4d7
kalkidan404 <kalrokal1621@gmail.com>
2026-01-31 21:56:11 +0300

    js basic logic

--- toe.js
+++ toe.js
@@ -1,52 +1,45 @@
-let position=[0,1,2,3,4,5,6,7,8];
-function game(choice,num){
-    
-    let Toprow=position[0,1,2];
-   let Middlerow=position[3,4,5];
-let Bottomrow=position[6,7,8]
-let Leftcolumn=position[0,3,6]
-let Middlecolumn=position[1,4,7]
-let Rightcolumn=position[2,5,8]
-let DiagonalTL=position[0,4,8]
-let DiagonalTR=position[2,4,6]
-    
-function gameboard(){
-    
-     if(num<0||num>8){
-            console.log("not appliable");
-        return;
-    }else if(position[num]==="x"||position[num]==="o"){
-            console.log("taken");
-        return;
-        }else{
-   
-console.log(`${num}=${choice}`);
-return position[num]=choice;
+const gameboard = {
+   board: [0,1,2,3,4,5,6,7,8],
+    num: null,
+    choice: null,
+    players: []
+};
+
+const gamestate=(function(){
+
+function getboard(){
+    return gameboard.board;
 }
+function mark(num){
+    gameboard.board[num]=gameboard.choice;
 }
-    function gamecontroller(){
-        if(Toprow===choice||Middlerow===choice||Bottomrow===choice||Leftcolumn===choice||Middlecolumn===choice||Rightcolumn===choice||DiagonalTL===choice||DiagonalTR===choice){
-            console.log(`${choice} is the winner`);
-    
-    }else if (position.every(cell => cell === "X" || cell === "O")) {
+function reset(){
+    gameboard.board=[0,1,2,3,4,5,6,7,8];
+} return{getboard,mark,reset}
+})();
+function player(name,choice){
+    return name,choice;
+}
+    const game=(function(){
+        if (
+  (gameboard.board[0] === gameboard.choice && gameboard.board[1] ===gameboard.choice && gameboard.board[2] === gameboard.choice) ||
+  (gameboard.board[3] === gameboard.choice &&gameboard.board[4] === gameboard.choice && gameboard.board[5] === gameboard.choice) ||
+  (gameboard.board[6] === gameboard.choice && gameboard.board[7] === gameboard.choice && gameboard.board[8] === gameboard.choice) ||
+  (gameboard.board[0] === gameboard.choice && gameboard.board[3] === gameboard.choice && gameboard.board[6] === gameboard.choice) ||
+  (gameboard.board[1] === gameboard.choice && gameboard.board[4] === gameboard.choice && gameboard.board[7] === gameboard.choice) ||
+  (gameboard.board[2] === gameboard.choice && gameboard.board[5] === gameboard.choice &&gameboard.board[8] === gameboard.choice) ||
+  (gameboard.board[0] ===gameboard.choice &&gameboard.board[4] === gameboard.choice && gameboard.board[8] === gameboard.choice) ||
+  (gameboard.board[2] ===gameboard.choice && gameboard.board[4] === gameboard.choice && gameboard.board[6] === gameboard.choice)
+) {
+  console.log(`${gameboard.choice} is the winner`);
+
+
+}else if (gameboard.board.every(cell => cell === "X" || cell === "O")) {
     console.log("All spaces are checked off — tie if no winner");
-     console.log(`${num}=${choice}`);
+     console.log(`${gameboard.num}=${gameboard.choice}`);
         console.log("it is a tie");
     }
-}return function(){
-    gameboard(),
-    gamecontroller()
-}
-}
-let kal=game("x",0);
-kal();
-let miki=game("o",4);
-miki();
-kal=game("x",1);
-kal();
-miki=game("o",2);
-miki();
-kal=game("x",7);
-kal();
- miki=game("o",6);
-miki();
+})();
+gamestate();
+game();
+player();
